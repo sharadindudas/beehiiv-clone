7ad8d0e50fa99ae42589b5ad61ad720853cd2d1c
Sharadindu Das <sharadindudas774@gmail.com>
2025-07-26 13:50:28 +0530

    fix: Remove routetree gen

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,6 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
-    component: () => (
-        <>
-            <Outlet />
-            <TanStackRouterDevtools initialIsOpen={false} />
-        </>
-    )
+    component: () => <Outlet />
 });
 
